60d28e2777aca5547f0122aadb8d4b9d3236b5b2
Sakib2263 <sakibhasan2263@gmail.com>
2019-11-26 13:10:10 +0600

    nothing much

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const ytdl = require('ytdl-core');
 const youtube = new YouTube(GOOGLE_API_KEY);
 const queue = new Map();
 
-var version ='1.0.1';
+var version ='1.0.2';
 
 bot.on('ready', () =>{
     console.log('This bot is born today!');
@@ -73,11 +73,12 @@ Please provide a value to select one of the search results ranging from 1-10.
 							time: 10000,
 							errors: ['time']
 						});
+						const videoIndex = parseInt(response.first().content);
 					} catch (err) {
 						console.error(err);
 						return msg.channel.send('No or invalid value entered, cancelling video selection.');
 					}
-					const videoIndex = parseInt(response.first().content);
+					//const videoIndex = parseInt(response.first().content);
 					var nvideo = await youtube.getVideoByID(videos[videoIndex - 1].id);
 				} catch (err) {
 					console.error(err);
